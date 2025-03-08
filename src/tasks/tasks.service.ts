d6599185ea3b7a13bdc3c5106f6214deda042b09
Biruk Moges <birukmoges8@gmail.com>
2025-03-08 15:52:48 +0300

    exception handling for get task by id and update status apis

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
-import { UpdateStatusDto } from './dto/update-status.dto';
+// import { UpdateStatusDto } from './dto/update-status.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Injectable()
@@ -34,7 +34,13 @@ export class TasksService {
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find((task) => task.id === id)!;
+    const found = this.tasks.find((task) => task.id === id)!;
+
+    if (!found) {
+      throw new NotFoundException(`Task with ID "${id}" not found`);
+    }
+
+    return found;
   }
 
   deleteTaskById(id: string): void {
