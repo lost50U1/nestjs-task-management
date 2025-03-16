d67f8d4fe2ec520f697e8430c914bec9ea3134ff
Biruk Moges <birukmoges8@gmail.com>
2025-03-16 20:30:16 +0300

    refactored the code

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -2,12 +2,12 @@ import { Module } from '@nestjs/common';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-// import { TasksRepository } from './task.repository';
+import { TasksRepository } from './task.repository';
 import { Task } from './task.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Task])],
   controllers: [TasksController],
-  providers: [TasksService],
+  providers: [TasksService, TasksRepository],
 })
 export class TasksModule {}
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,19 +1,14 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { TaskStatus } from './task-status.enum';
-// import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
 // import { UpdateStatusDto } from './dto/update-status.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
-// import { TasksRepository } from './task.repository';
-import { InjectRepository } from '@nestjs/typeorm';
+import { TasksRepository } from './task.repository';
 import { Task } from './task.entity';
-import { Repository } from 'typeorm';
+
 @Injectable()
 export class TasksService {
-  constructor(
-    @InjectRepository(Task)
-    private tasksRepository: Repository<Task>,
-  ) {}
+  constructor(private tasksRepository: TasksRepository) {}
   // private tasks: Task[] = [];
   // getAllTasks(): Task[] {
   //   return this.tasks;
@@ -68,28 +63,7 @@ export class TasksService {
   //   return task;
   // }
 
-  async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
-    const { title, description } = createTaskDto;
-
-    const task = this.tasksRepository.create({
-      title,
-      description,
-      status: TaskStatus.OPEN,
-    });
-
-    await this.tasksRepository.save(task);
-    return task;
+  createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+    return this.tasksRepository.createTask(createTaskDto);
   }
-
-  // createTask(createTaskDto: CreateTaskDto): Task {
-  //   const { title, description } = createTaskDto;
-  //   const task: Task = {
-  //     id: uuid(),
-  //     title,
-  //     description,
-  //     status: TaskStatus.OPEN,
-  //   };
-  //   this.tasks.push(task);
-  //   return task;
-  // }
 }
